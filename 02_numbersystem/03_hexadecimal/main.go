146db5ea595e597967c6646ec49af03ad7cf7130
Anurag Shendge <shendge.anurag@gmail.com>
2017-12-05 16:31:53 -0500

    check out the looping syntax

--- 02_numbersystem/03_hexadecimal/main.go
+++ 02_numbersystem/03_hexadecimal/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// this is the main function I am writing to start learning
+// Golang programming. Small steps you see
 func main() {
 	fmt.Printf("%d \t %b \t %#X\n", 42, 42, 42)
 }
